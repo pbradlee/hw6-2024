e013d1e03f8f2f6690b73f98cc8d53550d3976c6
pbradlee <143355993+pbradlee@users.noreply.github.com>
2024-04-07 14:23:48 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,10 +5,12 @@ var video;
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video=document.querySelector("#player1")
+
 	video.autoplay=false;
 	video.loop=false;
-	console.log('autoplay is set to '+ video.autoplay)
-	console.log('loop is set to '+ video.loop)
+
+	console.log("Autoplay is set to "+ video.autoplay)
+	console.log("Loop is set to "+ video.loop)
 });
 
 // PLAY BUTTON
@@ -16,7 +18,9 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+
 	let volume = video.volume * 100;
+
 	console.log(volume);
 	document.querySelector("#volume").textContent = volume + "%";
 });
@@ -32,15 +36,19 @@ document.querySelector("#pause").addEventListener("click", function() {
 // Slow the current video speed by 10% each time the button is clicked and log the new speed to the console
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow Down");
-	video.playbackRate *= 0.9;
-	console.log("New speed is " + video.playbackRate);
+
+	let vidSpeed = video.playbackRate;
+	video.playbackRate = vidSpeed * 0.9;
+	console.log("New speed is "+ video.playbackRate);
 });
 
 // SPEED UP
 // Increase the current video speed each time the button is clicked and log the new speed to the console. Change it by an amount proportional to the slow down. CHECK THIS!! If you slow down three times and then speed up three times you should be within 5 digits of 100% again.
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Speed Up");
-	video.playbackRate /= 0.9;
+
+	let vidSpeed = video.playbackRate;
+	video.playbackRate = vidSpeed / 0.9;
 	console.log("New speed is " + video.playbackRate);
 });
 
@@ -49,18 +57,24 @@ document.querySelector("#faster").addEventListener("click", function() {
 document.querySelector("#skip").addEventListener("click", function() {
 	console.log("Skip Ahead");
 
-	if ((video.currentTime + 10) <= video.duration) {
-		video.currentTime += 10;
+	let videoDuration = video.duration;
+	let videoCurrentTime = video.currentTime;
+
+	if ((videoCurrentTime + 10) <= videoDuration) {
+		video.currentTime = videoCurrentTime + 10;
 	}
 	else {
 		video.currentTime = 0;
 	}
+
 	console.log("Current time is " + video.currentTime);
 });
 
 // MUTE
 // Mute/unmute the video and update the text in the button
 document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute/Unmute");
+
 	if (video.muted) {
 		console.log("Unmute");
 		video.muted = false;
@@ -77,21 +91,23 @@ document.querySelector("#mute").addEventListener("click", function() {
 // Change the volume based on the slider and update the volume information
 document.querySelector("#slider").addEventListener("change", function() {
 	console.log("Volume Change via Slider");
-	console.log('The current volume is ' +document.querySelector("#slider").value);
-	video.volume = document.querySelector("#slider").value / 100;
-	document.querySelector("#volume").textContent = document.querySelector("#slider").value + "%";
+	let sliderValue = document.querySelector("#slider").value;
+
+	console.log('The current volume is ' +sliderValue);
+	video.volume = sliderValue / 100;
+	document.querySelector("#volume").textContent = sliderValue + "%";
 });
 
 // STYLED
 // Utilize the existing oldSchool class on the video element
 document.querySelector("#vintage").addEventListener("click", function() {
-	console.log("Old School");
+	console.log("Old School Style");
 	video.classList.add("oldSchool");
 });
 
 // ORIGINAL
 // Remove the oldSchool class from the video
 document.querySelector("#orig").addEventListener("click", function() {
-	console.log("Original");
+	console.log("Original Style");
 	video.classList.remove("oldSchool");
 });
